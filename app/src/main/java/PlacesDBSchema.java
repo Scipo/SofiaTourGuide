c91f3148384f38b9e884b425e01b5287ec30965c
Scipo <ivots@mail.bg>
2018-01-29 14:13:44 +0200

    Places database schema

---
+++ app/src/main/java/PlacesDBSchema.java
@@ -0,0 +1,18 @@
+
+public class PlacesDBSchema {
+    public static final class PlaceTable{
+        public  static final String NAME = "name";
+
+        public static final class Cols{
+            public static final String UUID = "uuid";
+            public static final String Code ="code";
+            public static final String Name = "name";
+            public static final String Description = "description";
+            public static final String Address = "address";
+            public static final String Schedule = "schedule";
+            public static final String Price = "0.0";
+            public static final Double Lat = 0.0;
+            public static final Double Lon = 0.0;
+        }
+    }
+}
