bd69125a9d60a767278d1749cfce24b24cd48e2c
Scipo <ivots@mail.bg>
2018-01-29 14:11:26 +0200

    PlacesBaseHelper create app database

---
+++ app/src/main/java/PlacesBaseHelper.java
@@ -0,0 +1,35 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class PlacesBaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION = 1;
+    private static  String DATABASE_NAME ="placeDBase.db";
+
+    public PlacesBaseHelper(Context context){
+        super(context,DATABASE_NAME,null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL("create table " + PlacesDBSchema.PlaceTable.NAME + "("
+                + "_id integer primary key autoincrement," +
+                PlacesDBSchema.PlaceTable.Cols.UUID + ", "+
+                PlacesDBSchema.PlaceTable.Cols.Code + ", "+
+                PlacesDBSchema.PlaceTable.Cols.Name + ", "+
+                PlacesDBSchema.PlaceTable.Cols.Description + ", "+
+                PlacesDBSchema.PlaceTable.Cols.Address + ", "+
+                PlacesDBSchema.PlaceTable.Cols.Schedule + ", " +
+                PlacesDBSchema.PlaceTable.Cols.Price + ", " +
+                PlacesDBSchema.PlaceTable.Cols.Lat + ", " +
+                PlacesDBSchema.PlaceTable.Cols.Lon +
+                ")"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db,int oldVersion, int newVersion){
+
+    }
+
+}
